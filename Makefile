74b848a4c5deeadb651be55527002d2515e02ee9
Tim B. <tim@bellfamily.com.au>
2013-03-28 07:36:51 +1000

    SSI progress.

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ TARGET = engg
 
 #### Setup ####
 STELLARISWARE = C:\StellarisWare
+CMSIS = C:\cmsis
 SRC           = $(wildcard src/*.c)
 TOOLCHAIN     = arm-none-eabi
 PART          = LM4F120H5QR
@@ -22,15 +23,19 @@ CFLAGS = -mthumb -mcpu=$(CPU) -mfpu=$(FPU) -mfloat-abi=$(FABI)
 CFLAGS+= -Os -ffunction-sections -fdata-sections
 CFLAGS+= -MD -std=c99 -Wall -pedantic
 CFLAGS+= -DPART_$(PART) -c -DTARGET_IS_BLIZZARD_RA1
-CFLAGS+= -g
+CFLAGS+= -g -D DEBUG
 
 LIB_GCC_PATH=$(shell $(CC) $(CFLAGS) -print-libgcc-file-name)
 LIBC_PATH=$(shell $(CC) $(CFLAGS) -print-file-name=libc.a)
 LIBM_PATH=$(shell $(CC) $(CFLAGS) -print-file-name=libm.a)
 DRIVER_LIB=$(STELLARISWARE)/driverlib/gcc-cm4f/libdriver-cm4f.a
 USB_LIB=$(STELLARISWARE)/usblib/gcc-cm4f/libusb-cm4f.a
+CMSIS_MATH=$(CMSIS)/CMSIS/Lib/GCC/libarm_cortexM4lf_math.a
+CMSIS_LIB=$(CMSIS)/CMSIS/Lib/libdsplib_lm4f.a
 
-CFLAGS+= -I $(STELLARISWARE) 
+CFLAGS+= -I$(STELLARISWARE) 
+CFLAGS+= -I$(CMSIS)/CMSIS/Include
+CFLAGS+= -I$(CMSIS)/Device/ARM/ARMCM4/Include
 
 LFLAGS = --gc-sections --entry ResetISR
 CPFLAGS = -Obinary
@@ -53,7 +58,7 @@ all: $(OBJS) $(TARGET).axf $(TARGET)
 $(TARGET).axf: $(OBJS)
 	@echo
 	@echo Linking...
-	$(LD) -T $(LINKER_FILE) $(LFLAGS) -o bin/$(TARGET).axf $(OBJS) $(DRIVER_LIB) $(LIBM_PATH) $(LIBC_PATH) $(LIB_GCC_PATH)
+	$(LD) -T $(LINKER_FILE) $(LFLAGS) -o bin/$(TARGET).axf $(OBJS) $(DRIVER_LIB) $(LIBM_PATH) $(LIBC_PATH) $(LIB_GCC_PATH) $(CMSIS_LIB)
 
 $(TARGET): $(TARGET).axf
 	@echo
--- src/main.c
+++ src/main.c
@@ -1,54 +1,85 @@
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
-
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
-#include "driverlib/timer.h"
-#include "driverlib/pin_map.h"
-#include <math.h>
-
-#include "driverlib/rom.h"
-#include "driverlib/sysctl.h"
+#include "driverlib/ssi.h"
 #include "utils/uartstdio.h"
+#include <inttypes.h>
+#include <stdio.h>
 
-int main(void) {
-    unsigned long ulPeriod, dutyCycle;
-
-    // 40 MHz system clock
-    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|
-        SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-
-    ulPeriod = 100;
-    dutyCycle = 0;
-
-    // Turn off LEDs
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-
-    // Configure PB6 as T0CCP0
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-    GPIOPinConfigure(GPIO_PB6_T0CCP0);
-    GPIOPinTypeTimer(GPIO_PORTB_BASE, GPIO_PIN_6);
+void dacWrite(uint16_t val) {
+    long unsigned int *nothing;
+    long unsigned int something = 0;
+    nothing = &something;
+    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0);
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, 0);
+   // UARTprintf("BEF\n");
+    SSIDataPut(SSI0_BASE, 0x31);
+    //SSIDataGet(SSI0_BASE, nothing);
+    //while(SSIBusy(SSI0_BASE)) { }
+    SSIDataPut(SSI0_BASE, val);
+    //SSIDataGet(SSI0_BASE, nothing);
+    //while(SSIBusy(SSI0_BASE)) { }
+    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, GPIO_PIN_3);
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, GPIO_PIN_5);
+    SysCtlDelay(200000);
+    UARTprintf("Test swiaft\n");
     
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
-    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    UARTStdioInit(0);    
+}
 
-    // Configure timer
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-    TimerConfigure(TIMER0_BASE, TIMER_CFG_SPLIT_PAIR|TIMER_CFG_A_PWM);
-    TimerLoadSet(TIMER0_BASE, TIMER_A, ulPeriod -1);
-    TimerMatchSet(TIMER0_BASE, TIMER_A, dutyCycle); // PWM
-    TimerEnable(TIMER0_BASE, TIMER_A);
-    while(1) { // The following code varies the duty cycle over time
-        dutyCycle++;
-        TimerMatchSet(TIMER0_BASE, TIMER_A, dutyCycle);
-        SysCtlDelay(1000);
-        if(dutyCycle > 256) {
-            dutyCycle = 0;
+int main() {
+
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+            SYSCTL_XTAL_16MHZ);
+
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralSleepEnable(SYSCTL_PERIPH_UART0);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTStdioInit(0);
+    UARTprintf("Test\n");
+
+    //
+    // Configure the SSI.
+    //
+    UARTprintf("Test1\n");
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI0);
+    UARTprintf("Test2\n");
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    //GPIOPinConfigure(GPIO_PA3_SSI0FSS);
+
+    GPIOPinConfigure(GPIO_PA2_SSI0CLK);
+    GPIOPinConfigure(GPIO_PA4_SSI0RX);
+    GPIOPinConfigure(GPIO_PA5_SSI0TX);
+    
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_5);
+    UARTprintf("Test6\n");
+    GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0);
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_5, 0);
+    UARTprintf("Test7\n");
+    GPIOPinTypeSSI(GPIO_PORTA_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_2);
+    UARTprintf("Test8\n");
+    SSIConfigSetExpClk(SSI0_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_0,
+            SSI_MODE_MASTER, 1000000, 0);
+    UARTprintf("Test9\n");
+    SSIEnable(SSI0_BASE);
+
+    UARTprintf("Test10\n");
+    //
+    // Send some data.
+    //
+    int val = 2047;
+    while (1) {
+        dacWrite(val);
+        SysCtlDelay(200000);
+        val++;
+        if (val > 4095) {
+            val = 0;
         }
     }
+
 }
